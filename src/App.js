6968f678704d91ad2d686df46a9866fe39b81c4c
lizageorge24 <lizageorge2407@gmail.com>
2026-06-03 00:17:51 -0400

    Searchfilter problem

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import React, { useState } from 'react';
-const products = [
-  { id: 1, name: 'Outback', type: 'SUV' },
-  { id: 2, name: 'Impreza', type: 'Sedan' },
-  { id: 3, name: 'Forester', type: 'SUV' },
-  { id: 4, name: 'Civic', type: 'Sedan' },
-];
-
-export default function ProductList() {
-  const [search, setSearch] = useState('');
-
-  const filtered = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase())
-  );
-
+import SearchFilter from "./SearchFilter";
+export default function App() {
   return (
     <div>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search Cars..."
-      />
-      <ul>
-        {filtered.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      < SearchFilter/>
     </div>
   );
 }
